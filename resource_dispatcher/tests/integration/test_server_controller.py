1a1d910833f0b41dacccad21af36ad6542f1e85b
misohu <michal.hucko@canonical.com>
2023-03-22 14:53:23 +0100

    KF-739 resource dispatcher source code

---
+++ resource_dispatcher/tests/integration/test_server_controller.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+# Copyright 2023 Canonical Ltd.
+# See LICENSE file for licensing details.
+#
+
+"""Integration tests for Resource Dispatcher server controller."""
+
+import json
+import threading
+from http.server import HTTPServer
+
+import pytest
+import requests
+
+from resource_dispatcher.src.server import server_factory
+
+PORT = 0  # HTTPServer randomly assigns the port to a free port
+LABEL = "test.label"
+FOLDER = "./resource_dispatcher/tests/test_data_folder"
+
+CORRECT_NAMESPACE_REQ = {
+    "parent": {"metadata": {"name": "someName", "labels": {LABEL: "true"}}},
+    "children": {
+        "Secret.v1": [],
+    },
+}
+
+WRONG_NAMESPACE_REQ = {
+    "parent": {"metadata": {"name": "someName", "labels": {"wrong.namespace": "true"}}},
+    "children": {
+        "Secret.v1": [],
+    },
+}
+
+CORRECT_NAMESPACE_RESP = {
+    "status": {"resources-ready": "False"},
+    "children": [
+        {
+            "apiVersion": "v1",
+            "kind": "Secret",
+            "metadata": {"name": "mlpipeline-minio-artifact2", "namespace": "someName"},
+            "stringData": {"AWS_ACCESS_KEY_ID": "value", "AWS_SECRET_ACCESS_KEY": "value"},
+        },
+        {
+            "apiVersion": "v1",
+            "kind": "Secret",
+            "metadata": {"name": "mlpipeline-minio-artifact", "namespace": "someName"},
+            "stringData": {"AWS_ACCESS_KEY_ID": "value", "AWS_SECRET_ACCESS_KEY": "value"},
+        },
+    ],
+}
+
+WRONG_NAMESPACE_RESP = {"status": {}, "children": []}
+
+
+@pytest.fixture(
+    scope="function",
+)
+def server():
+    """
+    Start the sync HTTP server for a given set of parameters. Create server on a separate thread.
+
+    Yields:
+    * the server (useful to interrogate for the server address)
+    """
+    server = server_factory(PORT, LABEL, FOLDER)
+    server_thread = threading.Thread(target=server.serve_forever)
+    server_thread.daemon = True
+    server_thread.start()
+    yield server
+
+
+@pytest.mark.parametrize(
+    "request_data, response_data",
+    [
+        (CORRECT_NAMESPACE_REQ, CORRECT_NAMESPACE_RESP),
+        (WRONG_NAMESPACE_REQ, WRONG_NAMESPACE_RESP),
+    ],
+)
+def test_server_responses(server: HTTPServer, request_data, response_data):
+    """Test if server returns desired Kubernetes objects for given namespaces."""
+    server_obj = server
+    url = f"http://{server_obj.server_address[0]}:{str(server_obj.server_address[1])}"
+    response = requests.post(url, data=json.dumps(request_data))
+    result = json.loads(response.text)
+    assert response.status_code == 200
+    assert result["status"] == response_data["status"]
+    assert [i for i in response_data["children"] if i not in result["children"]] == []
